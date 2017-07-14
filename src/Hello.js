eff3d81e7001c77248bc5b64eaf6328c73bda3b6
Michael Marcinek <michaelmarcinek@gmail.com>
2017-07-13 22:17:53 -0400

    frenchify

--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import './Hello.css'
+import React, { Component } from 'react';
+import './Hello.css';
 
-const Hello = (props) => {
-  return (
-    <div className="Hello">Hello {props.name}</div>
-  );
+class Hello extends Component {
+  constructor(props){
+    super(props)
+    this.state = { greeting: 'Hello'}
+  }
+
+  frenchify(){
+    if(this.state.greeting === "Hello"){
+      this.setState({greeting: 'Bonjour'});
+    }
+    else {
+      this.setState({greeting: 'Hello'})
+    }
+  }
+
+  render(){
+    return (
+      <div className="Hello">
+        {this.state.greeting} {this.props.name}
+        <br/>
+        <button onClick={this.frenchify = this.frenchify.bind(this)}>Frenchify</button>
+      </div>
+    );
+  }
 }
 
 export default Hello;
+
