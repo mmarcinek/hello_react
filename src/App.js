99813579384e2ca5d5491870fdd5fbddd5c5d0d2
Michael Marcinek <michaelmarcinek@gmail.com>
2017-07-13 21:33:28 -0400

    adds Hello.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello React</h1>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">Hello World!</div>
+  );
 }
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css'
+
+const Hello = () => {
+  return (
+    <div className="App">Hello React!</div>
+  );
+}
+
+export default Hello;
